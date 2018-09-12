f19c4f04be9c185a7136936067976b8e58e2c681
drakemain <demain1990@gmail.com>
2018-09-11 17:17:52 -0700

    send html to client

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+  for stream in listener.incoming() {
+    let stream = stream.unwrap();
 
-        handle_connection(stream);
-    }
+    handle_connection(stream);
+  }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+  let mut buffer = [0; 512];
 
-    stream.read(&mut buffer).unwrap();
+  stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+  let get = b"GET / HTTP/1.1\r\n";
+  
+  let (status_line, content_file) = if buffer.starts_with(get) {
+    ("HTTP/1.1 200 OK\r\n\r\n", "html/hello.html")
+  } else {
+    ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "html/404.html")
+  };
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+  let contents = fs::read_to_string(content_file).unwrap();
+  let response = format!("{}{}", status_line, contents);
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();  
 }
